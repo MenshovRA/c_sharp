7cac6820543d5496fc78fe67ebb8477d7f5977dc
Roman-PC <menshov.ra@yandex.ru>
2023-01-08 18:32:52 +0300

    Класс с формой WPF для хранения локальных настроек рабочих мест АРМ. Первый коммит.

---
+++ AwsLocalSettings/src/LocalSettingData.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AwsLocalSettings
+{
+	public class LocalSettingData
+	{
+		public class Employee 
+		{
+			// TODO: это внешний класс
+		}
+
+		public class WorkstationInfo : INotifyPropertyChanged
+		{
+			private string name;
+			public string Name 
+			{
+				get { return name; }
+				set
+				{
+					name = value;
+					OnPropertyChanged("Name");
+				}
+			}
+
+			private OperationType.Info operationType;
+			public OperationType.Info OperationType
+			{
+				get { return operationType; }
+				set
+				{
+					operationType = value;
+					OnPropertyChanged("OperationType");
+				}
+			}
+
+			private ConnectionType connectionType;
+			public ConnectionType ConnectionType
+			{
+				get { return connectionType; }
+				set
+				{
+					connectionType = value;
+					OnPropertyChanged("ConnectionType");
+				}
+			}
+
+			private bool enabled;
+			public bool Enabled 
+			{
+				get { return enabled; }
+				set
+				{
+					enabled = value;
+					OnPropertyChanged("Enabled");
+				}
+			}
+
+			public List<Employee> Emple;
+
+			public WorkstationInfo()
+			{
+				Emple = new List<Employee>();
+			}
+
+			public event PropertyChangedEventHandler PropertyChanged;
+			public void OnPropertyChanged([CallerMemberName] string prop = "")
+			{
+				if (PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			}
+		}
+
+		public List<WorkstationInfo> Workstation { get; set; }
+
+		public void ShowSettingPanel()
+		{
+			var form = new LocalSettingWindow();
+			
+			form.BtnApply.Click += (s, e) =>
+			{
+				form.Close();
+			};
+
+			form.BtnCancel.Click += (s, e) => form.Close();
+
+			form.BtnAddWorkstation.Click += (s, e) =>
+			{
+				Workstation.Add(new WorkstationInfo()
+				{
+					Name = string.Format("Рабочее место №{0:D}", Workstation.Count + 1),
+					OperationType = OperationType.GetData()[0],
+					ConnectionType = ConnectionType.SERIAL,
+					Enabled = false,
+				});
+				form.DgWorkstation.ItemsSource = null;
+				form.DgWorkstation.ItemsSource = Workstation;
+			};
+
+			form.BtnDelWorkstation.Click += (s, e) =>
+			{
+			};
+
+			Workstation.Add(new WorkstationInfo()
+			{
+				Name = string.Format("Рабочее место №{0:D}", Workstation.Count + 1),
+				OperationType = OperationType.GetData()[0],
+				ConnectionType = ConnectionType.SERIAL,
+				Enabled = false,
+			});
+			form.DgWorkstation.ItemsSource = Workstation;
+			form.ShowDialog();
+		}
+
+		public LocalSettingData(string store_path)
+		{
+			Workstation = new List<WorkstationInfo>();
+		}
+	}
+}
---
+++ AwsLocalSettings/src/guide/Guide.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AwsLocalSettings
+{
+	public enum ConnectionType
+	{
+		SERIAL,
+		TCP,
+	}
+
+	public static class OperationType
+	{
+		public class Info : INotifyPropertyChanged
+		{
+			private string name;
+			public string Name 
+			{
+				get { return name; }
+				set
+				{
+					name = value;
+					OnPropertyChanged("Name");
+				}
+			}
+
+			private int code;
+			public int Code
+			{
+				get { return code; }
+				set
+				{
+					code = value;
+					OnPropertyChanged("Code");
+				}
+			}
+
+			public event PropertyChangedEventHandler PropertyChanged;
+			public void OnPropertyChanged([CallerMemberName] string prop = "")
+			{
+				if (PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			}
+		}
+
+		private static List<Info> data = new List<Info>
+		{
+			new Info() { Name = "Вафельница", Code = 111 },
+			new Info() { Name = "Сборка", Code = 222 },
+		};
+
+		public static List<Info> GetData()
+		{
+			return data;
+		}
+	}
+}
